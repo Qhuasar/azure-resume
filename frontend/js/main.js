28500c66f48273f5796ad54209e9e28bb01b7ebe
Pedro Matos <pedroddematos@gmail.com>
2023-12-14 23:06:54 +0000

    Updated template with my info. Added a event

---
+++ frontend/js/main.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  getVisitCount();
+});
+
+const import_api = '';
+
+const getVisitCount = () => {
+  let count = 30;
+  fetch(import_api)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log('Website called function API');
+      count = response.count;
+      document.getElementById('counter').innerText = count;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
